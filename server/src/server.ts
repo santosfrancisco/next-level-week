0736bad1beff3fbbd2259746bdcdefba4538263d
Francisco Santos <fwfs1985@gmail.com>
2020-06-04 22:19:32 -0300

    wip

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
-const app = express();
 const PORT = process.env.port || 3333
 
-app.get('/', (req, res) => {
-  res.json({message: "Next Level Week"})
-});
+const app = express();
+app.use(express.json())
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`)
